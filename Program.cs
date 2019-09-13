d00629cb9d2b058df6f1aa893227471bff9048f7
ALVES Jose Espogeira <JPEALVES@sonae.pt>
2019-09-13 18:14:10 +0100

    stuff

--- Program.cs
+++ Program.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
+using Newtonsoft.Json;
 
 namespace PoC_TableStorageStuff
 {
@@ -9,34 +10,67 @@ namespace PoC_TableStorageStuff
     {
         static void Main(string[] args)
         {
-            var storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=teszetstorageaccount;AccountKey=4I0Yq9eiSwAkF1iup30unhyo4Z3b4b6dV9vwerhgep/CdlCF7TlvXXiyBsG+Ku1jSolDAdwFwUMu9fkBfo9jsw==;EndpointSuffix=core.windows.net");
+            var storageAccount = CloudStorageAccount.Parse("");
 
             var client = storageAccount.CreateCloudTableClient();
 
             var table = client.GetTableReference("employee");
 
-            table.CreateIfNotExistsAsync();
+            table.CreateIfNotExistsAsync().GetAwaiter().GetResult();
 
-            var emp = new Employee(1, "BillG");
+            var emp = new EmployeeEntity(1, 123, "BillGrand");
+            
+            //var insertOp = TableOperation.InsertOrReplace(emp);
+            var insertOp = TableOperation.Insert(emp);
 
-            var insertOp = TableOperation. .InsertOrReplace(emp);
+            table.ExecuteAsync(insertOp).GetAwaiter().GetResult();
 
-            table.ExecuteAsync(insertOp);
+            // var condition = TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "1");  
+            // var query = new TableQuery<EmployeeEntity>().Where(condition);  
+            // var lst = table.ExecuteQuery(query).GetAwaiter().GetResult();  
+
+
+            TableQuery<EmployeeEntity> query = new TableQuery<EmployeeEntity>().Where(
+                TableQuery.CombineFilters(
+                    TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, "1"),
+                    TableOperators.And,
+                    TableQuery.GenerateFilterCondition("RowKey", QueryComparisons.Equal, "123")));
+            
+            var retrievedSkypeId = table.ExecuteQuery(query).ToList().FirstOrDefault();
         }
-    }
 
-    public class Employee : TableEntity
-    {
-        public Employee(int clientId, int employeeId, string name)
+        public class EmployeeEntity : TableEntity
         {
-            PartitionKey = clientId.ToString();
-            RowKey = employeeId.ToString();
-            Name = name;
-        }
+            public EmployeeEntity()
+            { }
 
-        public string PartitionKey { get; }
-        public string RowKey { get; }
-        public string Name { get; }
+            public EmployeeEntity(int clientId, int employeeId, string name) : base(clientId.ToString(), employeeId.ToString())
+            {
+                Name = name;
+            }
+
+            public string Name { get; set; }
+
+
+            public override IDictionary<string, EntityProperty> WriteEntity(OperationContext operationContext)
+            {
+                // This line will write partition key and row key, but not Layout since it has the IgnoreProperty attribute
+                var x = base.WriteEntity(operationContext);
+
+                // Writing x manually as a serialized string.
+                x[nameof(this.Name)] = new EntityProperty(JsonConvert.SerializeObject(this.Name));
+                return x;
+            }
+
+            public override void ReadEntity(IDictionary<string, EntityProperty> properties, OperationContext operationContext)
+            {
+                base.ReadEntity(properties, operationContext);
+                if (properties.ContainsKey(nameof(this.Name)))
+                {
+                    this.Name = JsonConvert.DeserializeObject<string>(properties[nameof(this.Name)].StringValue);
+                }
+            }
+        }
     }
 }
 
